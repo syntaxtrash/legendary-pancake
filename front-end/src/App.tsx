c3a082ce80af4718a7d5cd820d035e9028e75a01
Aaron Aco <aaronacodev@gmail.com>
2024-02-20 05:24:49 +0800

    showing an alert

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,15 @@
-// import Alert from "./components/Alert";
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [showAlert, setShowAlert] = useState(false);
   return (
     <>
-      <Button color="success" onClick={() => console.log("button clicked!")}>
+      {showAlert && (
+        <Alert onClose={() => setShowAlert(false)}>This is an alert!</Alert>
+      )}
+      <Button color="success" onClick={() => setShowAlert(true)}>
         Click me!!
       </Button>
     </>
--- front-end/src/components/Alert.tsx
+++ front-end/src/components/Alert.tsx
@@ -2,13 +2,26 @@ import { ReactNode } from "react";
 
 interface AlertProps {
   children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({ children }: AlertProps) => {
+const Alert = ({ children, onClose }: AlertProps) => {
   return (
-    <div className="alert alert-primary" role="alert">
-      {children}
-    </div>
+    <>
+      <div
+        className="alert alert-warning alert-dismissible fade show"
+        role="alert"
+      >
+        {children}
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+          onClick={onClose}
+        ></button>
+      </div>
+    </>
   );
 };
 
