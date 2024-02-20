9c067548ae79863bf07a9f2ac3fd4d5212c02af7
Aaron Aco <aaronacodev@gmail.com>
2024-02-20 20:53:57 +0800

    update item quantity

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -8,15 +8,30 @@ function App() {
   //   },
   // });
 
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  // const [pizza, setPizza] = useState({
+  //   name: "Spicy Pepperoni",
+  //   toppings: ["Mushroom"],
+  // });
+
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "P1", quantity: 1 },
+      { id: 2, title: "P2", quantity: 2 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({
-      ...pizza,
-      toppings: [...pizza.toppings, "Cheeze"],
+    // setPizza({
+    //   ...pizza,
+    //   toppings: [...pizza.toppings, "Cheeze"],
+    // });
+
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: 2 } : item
+      ),
     });
   };
   return (
