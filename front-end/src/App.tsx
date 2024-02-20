20efd9669ac14c2e676b876cde633b82dc0f58c4
Aaron Aco <aaronacodev@gmail.com>
2024-02-21 07:46:12 +0800

    add form

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,21 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <ExpandableText maxChars={100}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-        deleniti corporis nemo architecto nostrum accusamus natus esse sunt, quo
-        illo asperiores ab dignissimos maiores dicta? Beatae tenetur quidem
-        atque saepe aliquid blanditiis, explicabo rerum accusamus consequuntur,
-        perspiciatis asperiores labore, non eligendi delectus minima deleniti
-        nisi deserunt. Qui, voluptatem odio? Labore quasi, aut quia optio esse
-        dicta ipsum excepturi quos earum ipsam, fuga harum cum voluptates autem
-        totam. Quibusdam magni laudantium minus, est, nulla nemo neque placeat
-        odit quasi illo, alias impedit mollitia. Possimus quaerat assumenda
-        impedit voluptatum deserunt, nam facere quia officiis veritatis ipsum
-        id, nesciunt pariatur minima doloribus incidunt.
-      </ExpandableText>
+      <Form></Form>
     </>
   );
 }
