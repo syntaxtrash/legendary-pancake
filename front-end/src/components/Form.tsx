640f8c624e33efa034c4769188a31740611b4c99
Aaron Aco <aaronacodev@gmail.com>
2024-02-21 07:49:48 +0800

    add form submit

--- front-end/src/components/Form.tsx
+++ front-end/src/components/Form.tsx
@@ -1,6 +1,12 @@
+import { FormEvent } from "react";
+
 const Form = () => {
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("Submitted..");
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
