a78706352221801c429a6e62514ce719e0571116
Aaron Aco <aaronacodev@gmail.com>
2024-02-20 20:48:10 +0800

    pizza exercise

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-    },
+  // const [game, setGame] = useState({
+  //   id: 1,
+  //   player: {
+  //     name: "John",
+  //   },
+  // });
+
+  const [pizza, setPizza] = useState({
+    name: "Spicy Pepperoni",
+    toppings: ["Mushroom"],
   });
 
   const handleClick = () => {
-    setGame({
-      ...game,
-      player: {
-        ...game.player,
-        name: "Bob",
-      },
+    setPizza({
+      ...pizza,
+      toppings: [...pizza.toppings, "Cheeze"],
     });
   };
   return (
