7016daa6d223ef8740b2861204f6241da00484d1
Aaron Aco <aaronacodev@gmail.com>
2024-02-21 08:09:23 +0800

    useRef

--- front-end/src/components/Form.tsx
+++ front-end/src/components/Form.tsx
@@ -1,29 +1,48 @@
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
 
 const Form = () => {
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log("Submitted..");
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name
-        </label>
-        <input id="name" type="text" className="form-control" />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="age" className="form-label">
-          Age
-        </label>
-        <input id="age" type="number" className="form-control" />
-      </div>
-      <button className="btn btn-primary" type="submit">
-        Submit
-      </button>
-    </form>
-  );
+	const nameRef = useRef<HTMLInputElement>(null);
+	const ageRef = useRef<HTMLInputElement>(null);
+	const person = { name: "", age: 0 };
+
+	const handleSubmit = (event: FormEvent) => {
+		event.preventDefault();
+		console.log("Submitted..");
+		if (nameRef.current !== null && ageRef.current !== null) {
+			person.name = nameRef.current.value;
+			person.age = parseInt(ageRef.current.value);
+			console.log(person);
+		}
+	};
+	return (
+		<form onSubmit={handleSubmit}>
+			<div className="mb-3">
+				<label htmlFor="name" className="form-label">
+					Name
+				</label>
+				<input
+					ref={nameRef}
+					id="name"
+					type="text"
+					className="form-control"
+				/>
+			</div>
+			<div className="mb-3">
+				<label htmlFor="age" className="form-label">
+					Age
+				</label>
+				<input
+					ref={ageRef}
+					id="age"
+					type="number"
+					className="form-control"
+				/>
+			</div>
+			<button className="btn btn-primary" type="submit">
+				Submit
+			</button>
+		</form>
+	);
 };
 
 export default Form;
