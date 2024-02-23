095f6989a86b2fb3f4a1b29c996dccc1734ab951
Aaron Aco <aaronacodev@gmail.com>
2024-02-23 11:13:43 +0800

    add clear expense form after submit

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -23,7 +23,14 @@ function App() {
 	return (
 		<>
 			<div className="mb-5">
-				<ExpenseForm />
+				<ExpenseForm
+					onSubmit={(expense) =>
+						setExpenses([
+							...expenses,
+							{ ...expense, id: expenses.length + 1 },
+						])
+					}
+				/>
 			</div>
 			<div className="mb-3">
 				<ExpenseFilter
