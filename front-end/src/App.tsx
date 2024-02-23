2981e2b351c9a9204fed5d1ccfc9a8b9c8500f8c
Aaron Aco <aaronacodev@gmail.com>
2024-02-23 10:30:16 +0800

    add expense table

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,27 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
-  return (
-    <>
-      <Form></Form>
-    </>
-  );
+	const [expenses, setExpenses] = useState([
+		{ id: 1, description: "aa", amount: 10, category: "Utility" },
+		{ id: 2, description: "bb", amount: 10, category: "Utility" },
+		{ id: 3, description: "cc", amount: 10, category: "Utility" },
+		{ id: 4, description: "dd", amount: 10, category: "Utility" },
+	]);
+
+	if (expenses.length === 0) {
+		return <p>Table is empty</p>;
+	}
+	return (
+		<>
+			<ExpenseList
+				expenses={expenses}
+				onDelete={(id) =>
+					setExpenses(expenses.filter((e) => e.id !== id))
+				}
+			/>
+		</>
+	);
 }
 
 export default App;
