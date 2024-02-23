c937683ab2c57c5b17395ffab18ef41b7ab4e422
Aaron Aco <aaronacodev@gmail.com>
2024-02-23 11:05:46 +0800

    add expense form validations

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
 
-export const categories = ["Grocery", "Utility", "Entertainment"];
 function App() {
 	const [selectedCategory, setCategory] = useState("");
 	const [expenses, setExpenses] = useState([
